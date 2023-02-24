1eace5f9aa97390c3e9d6eaa5bcb330a056f14e6
TUKAHEEBWARUHANGA IRAD <104001652+iroid2@users.noreply.github.com>
2023-02-24 16:47:03 +0300

    .

---
+++ script.js
@@ -0,0 +1,16 @@
+// $("#owl-demo").owlCarousel({
+//   loop: true,
+//   margin: 10,
+//   nav: true,
+//   responsive: {
+//     0: {
+//       items: 1,
+//     },
+//     600: {
+//       items: 3,
+//     },
+//     1000: {
+//       items: 4,
+//     },
+//   },
+// });
